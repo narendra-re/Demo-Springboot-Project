db5e70adc14efbb6a8237d30afd9f25fc0499799
NARENDRA REDDY <NARENDRA REDDY@DESKTOP-C4QPB42>
2022-11-23 15:06:16 +0530

    first

---
+++ discount/src/main/java/com/demo/discount/disserviceinterface/DiscountInterface.java
@@ -0,0 +1,15 @@
+package com.demo.discount.disserviceinterface;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface DiscountInterface {
+	
+
+public	double discount(double amount);
+
+
+
+
+	
+}
